196d9b6c161cacba9b5289c2d95f7454149e1160
Graham Murray <graham.murr@yahoo.ie>
2017-01-30 15:01:10 +0000

    setup travis build to run django view integration tests

---
+++ test/conftest.py
@@ -0,0 +1,31 @@
+"""
+This is the test setup file which is
+run before any tests and it used to
+configure django so it can be tested
+using pytest
+"""
+
+import os
+import django
+from django.conf import settings
+
+# Manually set django settings environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'extractor_api.settings')
+
+
+def pytest_configure():
+    settings.DEBUG = False
+    settings.DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'travis',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'TEST': {
+                'NAME': 'extractor_service_test',
+            },
+        }
+    }
+
+    django.setup()
--- test/test_api_views.py
+++ test/test_api_views.py
@@ -1,21 +1,13 @@
-import django
-import os
-from django.test import Client, utils, TestCase
+from django.test import Client, TestCase
 from api.models import JobType
 from django.http import JsonResponse
 
 
 class ApiViewsTest(TestCase):
 
-    def setUp(self):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "extractor_api.settings")
-        django.setup()
-        utils.setup_test_environment()
-        self.client = Client()
 
-        # Create JobTypes
-        type1 = JobType(description='LINKEDIN')
-        type1.save()
+    def setUp(self):
+        self.client = Client()
 
     def test_run_linkedin_throw_exception(self):
         response = self.client.post('/api/v1/linkedin', {})
@@ -24,11 +16,13 @@ class ApiViewsTest(TestCase):
         self.assertTrue(isinstance(response, JsonResponse))
 
     def test_run_linkedin_post(self):
+        job_type = JobType.objects.create(description='LINKEDIN')
+        job_type.refresh_from_db()
+
         payload = '{"username": "someusername", "password": "somepassword"}'
         response = self.client.post('/api/v1/linkedin', payload, content_type='text/plain;charset=UTF-8')
 
         self.assertEqual(200, response.status_code)
-        self.assertEqual('application/json', response.content_type)
 
         # Test job was created in db
         response = self.client.get('/api/v1/job/1')
