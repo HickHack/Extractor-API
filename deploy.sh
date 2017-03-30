89f3f2259f1ab4171d38b00f33ce9e68142bbc8a
Graham Murray <graham.murr@yahoo.ie>
2017-03-30 23:25:59 +0100

    Minor tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 function init() {
-    chmod +x ./dependencies
-    sudo mysql -u root -p < setup.sql
-    ./dependencies
+    chmod +x ~/Extractor-API/dependencies
+    sudo mysql -u root -p < ~/Extractor-API/setup.sql
+    ~/Extractor-API/dependencies
     launch
 }
 
 function launch() {
     sudo /etc/init.d/mysql start
     python manage.py migrate
-	python3 ~/Extractor-API/manage.py runserver 8000
+	python3 ~/Extractor-API/manage.py runserver 2>&1 | grep -v " 200 "
 }
 
 init
