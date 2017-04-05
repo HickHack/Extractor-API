2f358c3f9475131b9721dedcbb87925fdb0e59b6
Graham Murray <graham.murr@yahoo.ie>
2017-04-05 20:50:10 +0100

    Deployment WSGI dependency

--- setup.sql
+++ setup.sql
@@ -1,3 +1,5 @@
-CREATE DATABASE extractor_service
+CREATE DATABASE IF NOT EXISTS extractor_service;
+CREATE USER 'django'@'localhost' IDENTIFIED BY '';
+USE extractor_service;
 GRANT ALL PRIVILEGES ON extractor_service.* TO 'django'@'localhost';
 FLUSH PRIVILEGES;
