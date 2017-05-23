4227e685d12613d2d59f46d430c99eba4e95aa13
Graham Murray <graham.murr@yahoo.ie>
2017-05-23 11:41:57 +0100

    Clean up

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,4 @@
 import time
-from threading import Timer
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.views import exception_handler
@@ -45,32 +44,6 @@ def bad_request(request):
 
 def generate_timestamp():
     return round(time.time())
-
-
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer = None
-        self.interval = interval
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
 
 
 class ResponsePayload(object):
